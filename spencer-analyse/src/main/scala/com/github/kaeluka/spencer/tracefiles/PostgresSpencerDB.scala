c348ed2d6c94cb248467d600139bff2479bc4ee6
Stephan Brandauer <stephan.brandauer@it.uu.se>
2017-02-14 22:59:58 +0000

    ...

--- spencer-analyse/src/main/scala/com/github/kaeluka/spencer/tracefiles/PostgresSpencerDB.scala
+++ spencer-analyse/src/main/scala/com/github/kaeluka/spencer/tracefiles/PostgresSpencerDB.scala
@@ -422,7 +422,7 @@ class PostgresSpencerDB(dbname: String, startSpark: Boolean = true) extends Spen
       case e: PSQLException =>
         this.conn.commit()
 
-        //println(s"CREATE TABLE IF NOT EXISTS $cacheKey AS $sql ;")
+        println(s"CREATE TABLE IF NOT EXISTS $cacheKey AS $sql ;")
         this.conn.createStatement().execute(
           s"CREATE TABLE IF NOT EXISTS $cacheKey AS $sql ;")
         this.conn.commit()
@@ -457,7 +457,7 @@ class PostgresSpencerDB(dbname: String, startSpark: Boolean = true) extends Spen
       case Right(q) =>
         this.prepareCaches(q.precacheInnersSQL)
         this.getCachedOrRunQuery(q).close()
-        val result = this.getCachedOrRunSQL(s"getPercentages($query)".hashCode.toString.replace("-","_"),
+        val result = this.getCachedOrRunSQL("cache_perc_"+s"getPercentages($query)".hashCode.toString.replace("-","_"),
           s"""SELECT
               |  ROUND(100.0*COUNT(id)/(SELECT COUNT(id) FROM objects WHERE id > 4), 2)
               |FROM
